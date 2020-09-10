517a19096927ed4e198ee4ebbf83afc13745c941
TheSplatt-WEB <b-26_sokol@mail.ru>
2020-09-11 02:43:17 +0800

    fixed errors and added rate-star customisation

--- app/js/main.js
+++ app/js/main.js
@@ -3,10 +3,12 @@ $(function(){
         
     });
     $('.product__item-star').rateYo({
-        starWidth: '15px',
+        starWidth: '13px',
         rating: '4.5',
+        ratedFill: '#ffc000',
         readOnly: true,
-        // starSvg: '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><title>star_outline</title><path d="M12 15.422l3.75 2.25-0.984-4.266 3.328-2.906-4.406-0.375-1.688-4.031-1.688 4.031-4.406 0.375 3.328 2.906-0.984 4.266zM21.984 9.234l-5.438 4.734 1.641 7.031-6.188-3.75-6.188 3.75 1.641-7.031-5.438-4.734 7.172-0.609 2.813-6.609 2.813 6.609z"></path></svg>',
+        normalFill: 'transparent',
+        starSvg: '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M12 17.25l-6.188 3.75 1.641-7.031-5.438-4.734 7.172-0.609 2.813-6.609 2.813 6.609 7.172 0.609-5.438 4.734 1.641 7.031z"></path></svg>',
     });
 
     var mixer = mixitup('.relise__inner');
