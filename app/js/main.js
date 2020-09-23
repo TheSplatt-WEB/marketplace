cff63ef9e0c436da3221c6e36e5eb26a4dae1265
TheSplatt-WEB <b-26_sokol@mail.ru>
2020-09-24 03:38:16 +0800

    added single-product

--- app/js/main.js
+++ app/js/main.js
@@ -53,4 +53,12 @@ $(function(){
     $('.header__menu-btn').on('click', function(){
         $('.header__menu-list').slideToggle();
     });
+    $('.single-product__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.single-product__tabs').find('.tab-item').removeClass('active-tab').hide();
+            $('.single-product__tabs .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
 });
